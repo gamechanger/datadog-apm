0ab3e72da378f4013739eade10ed2d6635930e2b
Matt Casey <matt.casey@minibardelivery.com>
2019-12-09 14:20:48 -0500

    recursively callable mock

--- src/mock-tracer.ts
+++ src/mock-tracer.ts
@@ -1,6 +1,23 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { Tracer } from 'dd-trace';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const emptyFn = function() {};
+
+const callableHandlers = {
+    get: function <T, P extends keyof T>(_target: T, _prop: P, _receiver: any): T[P] {
+        const newMock = new Proxy(emptyFn, callableHandlers);
+        return newMock as any as T[P];
+    },
+
+    apply: function <T extends (...args: any) => any, A extends Parameters<T>>(_target: T, _thisArg: any, _args: A): ReturnType<T> {
+        const newMock = new Proxy(emptyFn, callableHandlers);
+        return newMock as any as ReturnType<T>;
+    },
+}
+
+const callableMock = new Proxy(emptyFn, callableHandlers);
+
 export const mockTracer = new Proxy({} as Tracer, {
     get<K extends keyof Tracer>(_target: Tracer, key: K) {
         console.warn(`Tried to access the DataDog tracer before the init function was called. Attempted to access property "${key}".`);
@@ -9,33 +26,6 @@ export const mockTracer = new Proxy({} as Tracer, {
             return true;
         }
 
-        if (key === 'scope') {
-            return () => ({
-                active: () => {},
-                activate: () => {},
-            });
-        }
-
-        if (
-            key === 'startSpan' ||
-            key === 'init' ||
-            key === 'use'
-
-        ) {
-            return () => ({});
-        }
-
-        if (key === 'wrap') {
-            return (_: any, f: any) => f;
-        }
-
-        if (
-            key === 'extract' ||
-            key === 'trace'
-        ) {
-            return () => null;
-        }
-
-        return undefined;
+        return callableMock;
     },
 });
