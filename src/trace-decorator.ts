b754efb4909c0b52dcf6e6566125ee91bdaedc3b
Miroslav Shubernetskiy <miroslav@miki725.com>
2021-07-19 11:40:55 -0400

    fixing lint errors

--- src/trace-decorator.ts
+++ src/trace-decorator.ts
@@ -138,7 +138,7 @@ const traceClass = (config?: TraceConfig) =>
  * @param config Optional configuration for the span that will be created for this trace.
  */
 // Going to rely on inferrence do its thing for this function
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function trace(config?: TraceConfig) {
     function traceDecorator(target: Constructor): void;
     function traceDecorator<T>(
